3b5ba4df944ea8e5ac307d9927e29d265e6d2318
Sadjad Hoodgar <sadjad.hoodgar@icloud.com>
2025-10-07 10:11:53 +0200

    class added.

---
+++ src/main/java/org/train/interval/MergeIntervals.java
@@ -0,0 +1,11 @@
+package org.train.interval;
+
+/**
+ * LeetCode #56 - Merge Intervals
+ * Link: <a href="https://leetcode.com/problems/merge-intervals/">...</a>
+ */
+public class MergeIntervals {
+    public int[][] solution(int[][] intervals){
+
+    }
+}
