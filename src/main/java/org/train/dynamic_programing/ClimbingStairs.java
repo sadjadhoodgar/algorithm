2548f22edf1274997453622b0eea3ba894ec687f
Sadjad Hoodgar <sadjad.hoodgar@icloud.com>
2025-10-10 23:53:05 +0200

    #70. Climbing Stairs

---
+++ src/main/java/org/train/dynamic_programing/ClimbingStairs.java
@@ -0,0 +1,24 @@
+package org.train.dynamic_programing;
+
+/**
+ * LeetCode #70. Climbing Stairs
+ * Link: <a href="https://leetcode.com/problems/climbing-stairs/">...</a>
+ */
+public class ClimbingStairs {
+    int solution(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        int oneStepBefore = 2, twoStepBefore = 1;
+
+        for (int i = 3; i <= n; i++) {
+            int current = oneStepBefore + twoStepBefore;
+
+            twoStepBefore = oneStepBefore;
+            oneStepBefore = current;
+        }
+
+        return oneStepBefore;
+    }
+}
