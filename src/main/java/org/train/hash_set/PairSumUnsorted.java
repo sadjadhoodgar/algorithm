f30986497c8988767d9c4a685a6e88fb4f7342c9
Sadjad Hoodgar <sadjad.hoodgar@icloud.com>
2025-11-15 15:12:26 +0100

    two seum unsorted leet code 1

--- src/main/java/org/train/hash_set/PairSumUnsorted.java
+++ src/main/java/org/train/hash_set/PairSumUnsorted.java
@@ -1,10 +1,23 @@
 package org.train.hash_set;
 
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * LeetCode #1 - Two Sum
  * Link: <a href="https://leetcode.com/problems/two-sum/">...</a>
  */
 public class PairSumUnsorted {
+    public int[] solution(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
 
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(-nums[i])){
+                return new int[]{map.get(nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
 }
